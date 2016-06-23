cc6ca8291ead7eea55e44ec7ff8fd6f87df06b71
Viktor Stískala <viktor@stiskala.cz>
2016-06-23 22:44:20 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-shared-session',
-    version='0.1',
-    packages=['shared_session'],
+    version='0.2',
+    packages=['shared_session', 'shared_session.templatetags'],
     include_package_data=True,
     license='MPL',
     description='Django session sharing across multiple domains running same application',
+    url='https://github.com/ViktorStiskala/django-shared-session',
     author='Viktor Stískala',
     author_email='viktor@stiskala.cz',
-    install_requires=['django>=1.7', 'python-dateutil>=2.5', 'PyNaCl>=1.0.0'],
+    install_requires=['setuptools-git', 'django>=1.7', 'python-dateutil>=2.5', 'PyNaCl>=1.0.0'],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
