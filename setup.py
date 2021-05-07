108197b5c90a6b97ba3fe84c4b1213bd4272ff13
Viktor Stískala <viktor@stiskala.cz>
2021-05-07 19:38:40 +0200

    Added PyPi compatible long_description

--- setup.py
+++ setup.py
@@ -5,13 +5,18 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), 'r') as f:
+    long_description = f.read()
+
 setup(
     name='django-shared-session',
-    version='0.5.2',
+    version='0.5.3',
     packages=['shared_session', 'shared_session.templatetags'],
     include_package_data=True,
     license='MPL',
     description='Django session sharing across multiple domains running same application',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ViktorStiskala/django-shared-session',
     author='Viktor Stískala',
     author_email='viktor@stiskala.cz',
